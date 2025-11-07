89876ca6edced0a60a39bd8ebc1b47ec2a62aa89
Dayeon Lee <iamdayeonlee@gmail.com>
2025-11-07 00:01:06 +0000

    Initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,142 @@
 import Image from "next/image";
 
+import fallback from "../public/fallback.png";  
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  <div className="min-h-screen bg-gray-700">
+  <nav className="top-0 left-0 w-full mt-4 z-50 p-8">
+    <ul className="flex justify-center mx-w-7xl gap-8 mr-8 text-sm font-medium">
+      <li>Github</li>
+      <li>Linkedin</li>
+      <li>Download CV</li>
+    </ul>
+  </nav>
+  <div className="flex flex-col items-center justify-center max-w-4xl mx-auto p-4 space-y-8">
+    <section className="bg-pink-400">
+      <h1 className="text-5xl sm:text-6xl mb-4">Hero title</h1>
+      <p className="">Hero description - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+    </section>
+    <section className="w-full bg-amber-400">
+     <h2 className="text-3xl sm:text-4xl mb-4">about</h2>
+     <p className="">description - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+    </section>
+    <section className="w-full bg-amber-700">
+      <h2 className="text-3xl sm:text-4xl mb-4">skills</h2>
+      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 mb-4">
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="TypeScript" width={50} height={50} />
+           <p>TypeScript</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="JavaScript" width={50} height={50} />
+           <p>JavaScript</p>
         </div>
-      </main>
-    </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="React" width={50} height={50} />
+           <p>React</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Next.js" width={50} height={50} />
+           <p>Next.js</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Node.js" width={50} height={50} />
+           <p>Node.js</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Express.js" width={50} height={50} />
+           <p>Express.js</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Jest" width={50} height={50} />
+           <p>Jest</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="MySQL" width={50} height={50} />
+           <p>MySQL</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="PostgreSQL" width={50} height={50} />
+           <p>PostgreSQL</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="MongoDB" width={50} height={50} />
+           <p>MongoDB</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Supabase" width={50} height={50} />
+           <p>Supabase</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Tailwind CSS" width={50} height={50} />
+           <p>Tailwind CSS</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Docker" width={50} height={50} />
+           <p>Docker</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Tailwind CSS" width={50} height={50} />
+           <p>Python</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={fallback} alt="Docker" width={50} height={50} />
+           <p>Django</p>
+        </div>
+      </div>
+       <p className="text-center">And More...</p>
+    </section>
+    <section className="w-full bg-amber-900">
+      <h2 className="text-3xl sm:text-4xl mb-4">My projects</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="border-2 border-black">
+          <Image src={fallback} alt="project1" width={400} height={300} />
+          <div>
+            <h3 className="text-xl sm:text-2xl font-bold">project1 title</h3>
+            <p>2025</p>
+            <p>project1 description - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          <div className="flex gap-2 m-4 flex-wrap">
+            <div className="bg-gray-300 px-2 rounded-md text-gray-700">skill1</div>
+            <div>skill2</div>
+            <div>skill3</div>
+            <div>skill4</div>
+            <div>skill5</div>
+            <div>skill1</div>
+            <div>skill2</div>
+            <div>skill3</div>
+            <div>skill4</div>
+            <div>skill5</div>            
+          </div>
+          <div className="flex gap-4 m-4 justify-center">
+            <button className="flex items-center rounded-sm bg-gray-700 p-2 h-8"> Demo </button>
+            <button className="flex items-center rounded-sm bg-gray-700 p-2 h-8"> Github </button>
+          </div>
+        </div> 
+        <div className="border-2 border-black">project2</div>
+        <div className="border-2 border-black">project3</div>
+        <div className="border-2 border-black">project4</div>
+      </div>
+    </section>
+    <section className="w-full bg-blue-200">
+      <h2 className="text-3xl sm:text-4xl mb-4">Experiences</h2>
+      <div className="">
+        <div className="text-xl sm:text-2xl font-bold">experience1</div>
+        <div className="text-xl sm:text-2xl font-bold">experience2</div>
+        <div className="text-xl sm:text-2xl font-bold">experience3</div>
+        <div className="text-xl sm:text-2xl font-bold">experience4</div>
+      </div>
+    </section>
+    <section className="w-full bg-blue-400">
+      <h2 className="text-3xl sm:text-4xl mb-4">Get in touch</h2>
+      <div className="text-xl sm:text-2xl font-bold text-center">Download my <a href="/path/to/cv" className="text-blue-600 underline">CV</a></div>
+      <div className="flex justify-center gap-8 m-4">
+        <button className="flex items-center rounded-sm bg-gray-700 p-2 h-8"> Demo </button>
+        <button className="flex items-center rounded-sm bg-gray-700 p-2 h-8"> Github </button>
+      </div>
+    </section>
+  </div>
+</div>
   );
 }
